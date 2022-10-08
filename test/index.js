16c32aa88f70243a0f88f772ec4d3368b7d80c53
gagik894 <69167135+gagik894@users.noreply.github.com>
2022-10-08 13:01:14 +0400

    Redeploy

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
-
+//change
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
 // The file token.json stores the user's access and refresh tokens, and is
